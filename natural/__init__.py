2bf28a6414dbed3bf4ab25b475fff5e2252a2260
romyache <romluk2007@gmail.com>
2026-05-23 20:05:20 +0300

    init is done

---
+++ natural/__init__.py
@@ -0,0 +1,15 @@
+from natural.helpers import from_int, to_int, to_str
+from natural.COM_NN_D import COM_NN_D    # N-1
+from natural.NZER_N_B import NZER_N_B    # N-2
+from natural.ADD_1N_N import ADD_1N_N    # N-3
+from natural.ADD_NN_N import ADD_NN_N    # N-4
+from natural.SUB_NN_N import SUB_NN_N    # N-5
+from natural.MUL_ND_N import MUL_ND_N    # N-6
+from natural.MUL_Nk_N import MUL_Nk_N    # N-7
+from natural.MUL_NN_N import MUL_NN_N    # N-8
+from natural.SUB_NDN_N import SUB_NDN_N  # N-9
+from natural.DIV_NN_Dk import DIV_NN_Dk  # N-10
+from natural.DIV_NN_N import DIV_NN_N    # N-11
+from natural.MOD_NN_N import MOD_NN_N    # N-12
+from natural.GCF_NN_N import GCF_NN_N    # N-13
+from natural.LCM_NN_N import LCM_NN_N    # N-14
